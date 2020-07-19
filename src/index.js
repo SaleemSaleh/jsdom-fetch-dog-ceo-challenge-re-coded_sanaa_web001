10c751b7a632949bd22725ff2ea3966a735c3eae
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-19 13:00:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-fetch(imgUrl)
-  .then(resp => resp.json())
-  .then(json => renderBooks(json));
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+function fetchT(url,fuction){
+
+  fetch(url)
+    .then(resp => resp.json())
+    .then(json => fuction(json));
+  
+}
+
+ fetchT(imgUrl,renderBooks);
+
 
   function renderBooks(images) {
   const div = document.querySelector('#dog-image-container')
@@ -11,5 +20,5 @@ fetch(imgUrl)
   for (const element of images) {
       let img = document.createElement('img');
       img.src = element;
-        div.appendChild(img);
+      div.appendChild(img);
   }
